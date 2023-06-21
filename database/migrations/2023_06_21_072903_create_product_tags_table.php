fb98ff774b6ee2ce9cd1cf3fdff599f1f12889f6
Javad Mofateh <mhmdjwadmfth2@gmail.com>
2023-06-21 11:37:27 +0330

    Create Models and Migrations Part 2

---
+++ database/migrations/2023_06_21_072903_create_product_tags_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_tags', function (Blueprint $table) {
+
+            // Foreign Key To Connect To Tags Table
+            $table->foreignId('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade')->onUpdate('cascade');
+
+            // Foreign Key To Connect To Products Table
+            $table->foreignId('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->primary(['tag_id', 'product_id']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_tags');
+    }
+};
