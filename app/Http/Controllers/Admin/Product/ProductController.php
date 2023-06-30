7d7799a51001ec0a9c9d281a56ba7b40f2ca0132
Javad Mofateh <mhmdjwadmfth2@gmail.com>
2023-06-30 18:31:19 +0330

    Create Product Part 10 (Upload Images)

--- app/Http/Controllers/Admin/Product/ProductController.php
+++ app/Http/Controllers/Admin/Product/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin\Product;
 
+use App\Http\Controllers\Admin\Product\ProductImage\ProductImageController;
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use App\Models\Category;
@@ -35,28 +36,32 @@ class ProductController extends Controller
     public function store(Request $request)
     {
 
-        $request->validate([
-            'name' => 'required|string',
-            'brand_id' => 'required',
-            'tag_ids' => 'required',
-            'tag_ids.*' => 'required',
-            'is_active' => 'required',
-            'description' => 'nullable|string',
-            'primary_image' => 'required|mimes:jpg,jpeg,png,svg',
-            'images' => 'required', // در ابتدا خود آرایه باید وجود داشته باشد
-            'images.*' => 'mimes:jpg,jpeg,png,svg', // بعد هر کدام از فیلد های آن آرایه هم باید وجود داشته باشند
-            'category_id' => 'required',
-            'attribute_ids' => 'required',
-            'attribute_ids.*' => 'required',
-            'variation_values' => 'required', // در ابتدا باید خود آرایه وجود داشته باشد
-            'variation_values.*.*' => 'required', // برای هر کدام از آرایه های متغییر و مقادیر درون آن آرایه ها
-            'variation_values.price.*' => 'integer', // هر کدام از فیلد های آرایه ی قمیت که در آرایه ی متغیر قرار دارد باید از نوع عدد باشد
-            'variation_values.quantity.*' => 'integer', // هر کدام از فیلد های آرایه ی تعداد که در آرایه ی متغیر قرار دارد باید از نوع عدد باشد
-            'delivery_amount' => 'nullable|integer',
-            'delivery_amount_per_product' => 'nullable|integer'
-        ]);
+        // $request->validate([
+        //     'name' => 'required|string',
+        //     'brand_id' => 'required',
+        //     'tag_ids' => 'required',
+        //     'tag_ids.*' => 'required',
+        //     'is_active' => 'required',
+        //     'description' => 'nullable|string',
+        //     'primary_image' => 'required|mimes:jpg,jpeg,png,svg',
+        //     'images' => 'required', // در ابتدا خود آرایه باید وجود داشته باشد
+        //     'images.*' => 'mimes:jpg,jpeg,png,svg', // بعد هر کدام از فیلد های آن آرایه هم باید وجود داشته باشند
+        //     'category_id' => 'required',
+        //     'attribute_ids' => 'required',
+        //     'attribute_ids.*' => 'required',
+        //     'variation_values' => 'required', // در ابتدا باید خود آرایه وجود داشته باشد
+        //     'variation_values.*.*' => 'required', // برای هر کدام از آرایه های متغییر و مقادیر درون آن آرایه ها
+        //     'variation_values.price.*' => 'integer', // هر کدام از فیلد های آرایه ی قمیت که در آرایه ی متغیر قرار دارد باید از نوع عدد باشد
+        //     'variation_values.quantity.*' => 'integer', // هر کدام از فیلد های آرایه ی تعداد که در آرایه ی متغیر قرار دارد باید از نوع عدد باشد
+        //     'delivery_amount' => 'nullable|integer',
+        //     'delivery_amount_per_product' => 'nullable|integer'
+        // ]);
 
-        dd($request->all());
+        $productImageController = new ProductImageController();
+        $fileNameImages = $productImageController->upload($request->primary_image, $request->images);
+
+        dd($fileNameImages);
+
     }
 
     /**
---
+++ app/Http/Controllers/Admin/Product/ProductImage/ProductImageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Product\ProductImage;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductImageController extends Controller
+{
+    public function upload($primaryImage, $images)
+    {
+        $fileNamePrimaryImage = generateFileName($primaryImage->getClientOriginalName());
+        $primaryImage->move(public_path(env('PRODUCT_IMAGES_UPLOAD_PATH')), $fileNamePrimaryImage);
+
+
+        // برای ذخیره ی تصاویر فرعی چون با یک ارایه از تصاویر طرف هستیم باید به روش زیر عمل کنیم
+        $fileNameImages = [];
+        foreach($images as $image) {
+            $fileNameImage = generateFileName($image->getClientOriginalName());
+            $image->move(public_path(env('PRODUCT_IMAGES_UPLOAD_PATH')), $fileNameImage);
+
+            array_push($fileNameImages , $fileNameImage);
+        }
+
+        return [
+            'fileNamePrimaryImage' => $fileNamePrimaryImage,
+            'fileNameImages' => $fileNameImages
+        ];
+
+    }
+}
